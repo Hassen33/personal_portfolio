56dc2aa97ebb1551461e917d48dee3f55c3307a5
hassen <hassenbj33@gmail.com>
2022-06-28 14:14:38 +0100

    Higher_Order_Component

--- front/src/container/About/About.jsx
+++ front/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-// import { AppWrap, MotionWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
 import { urlFor, client } from '../../client';
 
@@ -45,4 +45,5 @@ const About = () => {
 //   'about',
 //   'app__whitebg',
 // );
-export default About;
+export default AppWrap(About, 'about');
+// export default About;

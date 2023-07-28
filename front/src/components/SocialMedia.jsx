397bbbfe13f0bdbdddadd113fae32b943077e35d
hassen <hassenbj33@gmail.com>
2023-07-28 12:03:24 +0100

    fix social media links

--- front/src/components/SocialMedia.jsx
+++ front/src/components/SocialMedia.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { BsTwitter, BsLinkedin, BsGithub } from 'react-icons/bs';
-
+import React from "react";
+import { BsBehance, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsGithub />
+        <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+        <a target="_blank" href="https://github.com/Hassen33">
+          <BsGithub />
+        </a>
       </div>
       <div>
-        <BsLinkedin />
+        <a  target="_blank" href="https://www.linkedin.com/in/hassen-bejaoui">
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <BsTwitter />
+        <a target="_blank" href="https://www.behance.net/hassenbejaoui1">
+          <BsBehance />
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default SocialMedia;
